a726acce8a58b256f4c169a11abc972fbfdcae6e
Sami Benyelles <benyelles82@hotmail.com>
2021-11-15 10:06:44 +0400

    first models changes

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,3 +1,31 @@
 class SubscriptionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
+  def index
+    @subscriptions = Subscription.all
+  end
+
+  def new
+    @subscription = Subscription.new
+    authorize @subscription
+  end
+
+  def create
+    @subscription = Subscription.create(subscription_params)
+    @subscription.user = current_user
+    authorize @subscription
+
+    if @subscription.save
+      redirect_to @subscription, notice: 'subscription was successfully created.'
+    else
+      render :news
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :username, :type, :notify, :notes,
+                                        :notify_before, :price, :start_date, :end_date)
+  end
 end
