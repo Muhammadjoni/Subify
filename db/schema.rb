7cea1b1dc02ebd11d992c631b9ea8d0307d462a0
Janyl Ismailova <ismailovaj@yahoo.com>
2021-11-15 12:29:20 +0400

    seeding done plus type category changed to sub_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_14_114436) do
+ActiveRecord::Schema.define(version: 2021_11_15_080341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_14_114436) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.string "username"
-    t.string "type"
+    t.string "sub_type"
     t.date "start_date"
     t.date "end_date"
     t.integer "price"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,78 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up database..."
+User.destroy_all
+Subscription.destroy_all
+puts "Database cleaned"
+
+puts "Creating the Users"
+p user = User.create(
+  first_name: 'Bruce',
+  last_name: 'Wayne',
+  email: 'bruce.wayne@gotham.com',
+  password: 'batman',
+  sms_number: '+971589199329',
+  whatsapp_number: '+971589199329'
+)
+
+puts "Done with the Users Creation"
+
+puts "Creating the subscriptions"
+
+user.subscriptions.create(
+  title: 'Netflix Standard',
+  start_date: '2021-02-03',
+  end_date: '2022-02-03',
+  sub_type: 'monthly',
+  notify_before: 1,
+  price: 30,
+  currency: 'USD',
+  notify: true,
+  category: 'Entertainment'
+)
+
+user.subscriptions.create(
+  title: 'Amazon Prime',
+  start_date: '2020-08-03',
+  end_date: '2022-08-02',
+  sub_type: 'annual',
+  notify_before: 1,
+  price: 20,
+  currency: 'USD',
+  notify: true,
+  category: 'Entertainment'
+)
+
+user.subscriptions.create(
+  title: 'Coursera Plus',
+  start_date: '2021-01-01',
+  end_date: '2021-12-12',
+  sub_type: 'annual',
+  notify_before: 1,
+  price: 15,
+  currency: 'EUR',
+  notify: true,
+  category: 'Education'
+)
+
+user.subscriptions.create(
+  title: 'edX',
+  start_date: '2021-01-01',
+  end_date: '2022-12-12',
+  sub_type: 'annual',
+  notify_before: 1,
+  price: 15,
+  currency: 'GBP',
+  notify: true,
+  category: 'Education'
+)
+
+user.subscriptions.create(
+  title: 'Water usage bills',
+  start_date: '2021-01-01',
+  end_date: '2025-12-01',
+  sub_type: 'weekly',
+  notify_before: 1,
+  price: 20,
+  currency: 'AED',
+  notify: true,
+  category: 'Utility bills'
+)
