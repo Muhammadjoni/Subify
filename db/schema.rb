31ea1d068258b2ad20898f3a2fe3329728117310
Sami Benyelles <benyelles82@hotmail.com>
2021-11-17 11:42:23 +0400

    4th day branch

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_132332) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "category"
-    t.string "currency"
+    t.text "category"
+    t.text "currency"
     t.date "notification_date"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
