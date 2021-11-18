0b3b7565896e39cd090d1a117d3bc24c9f8e7524
Julia Chirkova <polmatmira@gmail.com>
2021-11-18 12:05:52 +0400

    resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_132332) do
+ActiveRecord::Schema.define(version: 2021_11_18_055600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
@@ -29,8 +50,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_132332) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "category"
-    t.string "currency"
+    t.text "category"
+    t.text "currency"
     t.date "notification_date"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
@@ -47,9 +68,11 @@ ActiveRecord::Schema.define(version: 2021_11_16_132332) do
     t.string "last_name"
     t.text "sms_number"
     t.text "whatsapp_number"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "subscriptions", "users"
 end
