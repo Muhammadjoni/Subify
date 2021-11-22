b2739a22310d8032dc3d4533ea7e9afad3eab40c
Jon <62991331+Muhammadjoni@users.noreply.github.com>
2021-11-22 19:38:44 +0400

    removed external libraries from datepicker
    Need to see how to flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// require jquery - 3.2.1.slim.min
-// require popper.min
-// require bootstrap.min
-// require bootstrap-datepicker/core
-// require bootstrap-datepicker/locales/bootstrap-datepicker.en-GB.js
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
